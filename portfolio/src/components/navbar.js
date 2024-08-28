d2bcbcc9c39995a9def8f9e47ba326f085517855
Teh Peh <153953294+TehPeh01@users.noreply.github.com>
2024-08-28 18:53:32 -0400

    finished navbar

--- portfolio/src/components/navbar.js
+++ portfolio/src/components/navbar.js
@@ -1,9 +1,19 @@
 import React from "react";
 import './navbar.css';
-
+import { Link } from 'react-scroll';
+ 
 const Navbar = () => {
     return (
-        <div>navbar</div>
+        <nav className="navbar">
+            <img src="" alt="Logo" className="logo"/>
+            <div className="desktopMenu">
+                <Link className="desktopMenuListItem">Home</Link>
+                <Link className="desktopMenuListItem">About</Link>
+                <Link className="desktopMenuListItem">Skills</Link>
+                <Link className="desktopMenuListItem">Projects</Link>
+            </div>
+            <button className="desktopMenuBtn">Contact Me</button>
+        </nav>
     )
 }
 
